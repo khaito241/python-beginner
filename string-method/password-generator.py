ad263df6ac7d8aeb067e549353cb460fdb90f7a9
Khai To <quangkhai.to24@gmail.com>
2021-01-25 01:18:58 +0700

    random password generator

---
+++ string-method/password-generator.py
@@ -0,0 +1,35 @@
+'''
+Project: Random Password Generator
+
+Password: 
+'''
+
+import string
+import random
+
+LETTERS = string.ascii_letters
+NUMBERS = string.digits
+PUNCTUATION = string.punctuation
+
+def password_generator(length=8):
+    printable = f'{LETTERS}{NUMBERS}{PUNCTUATION}'
+
+    printable = list(printable) #change printable to list so random() is operatable
+    random.shuffle(printable)
+
+    random_password = random.choices(printable, k=length)
+    random_password = ''.join(random_password)
+    return random_password
+
+def get_password_length():
+    password_length = input("How long do you want your password: ")
+    return int(password_length)
+
+def main():
+    password_length = get_password_length()
+    password = password_generator(password_length)
+
+    print(password)
+
+if __name__ == '__main__':
+    main()
